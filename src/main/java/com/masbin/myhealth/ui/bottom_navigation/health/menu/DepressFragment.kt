381d9ca62792376ea12a0bce56d0793ae02a7b30
muhammadbintangkh <muhammad.bintang.khatulistiwa@hashmicro.com>
2023-07-30 11:47:33 +0700

    [IMP] bisa namppilin data dari BE

--- src/main/java/com/masbin/myhealth/ui/bottom_navigation/health/menu/DepressFragment.kt
+++ src/main/java/com/masbin/myhealth/ui/bottom_navigation/health/menu/DepressFragment.kt
@@ -1,60 +1,103 @@
+// DepressFragment.kt
 package com.masbin.myhealth.ui.bottom_navigation.health.menu
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.masbin.myhealth.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DepressFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DepressFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var depressionLevelTextView: TextView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // Define the DepressionData class inside DepressFragment
+    data class DepressionData(val level: Int, val userId: Int)
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_depress, container, false)
+        val view = inflater.inflate(R.layout.fragment_depress, container, false)
+        depressionLevelTextView = view.findViewById(R.id.valueDepress)
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DepressFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DepressFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Fetch depression level data from the Flask server and display it in the TextView
+        fetchDataFromServer()
+    }
+
+    private fun fetchDataFromServer() {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://beflask.as.r.appspot.com//get/depress/history") // Adjust the Flask endpoint for depression level data
+            .build()
+
+        GlobalScope.launch(Dispatchers.IO) {
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    // Handle errors when fetching data from the server
+                    // For example, display an error message or log for debugging
+                    e.printStackTrace()
                 }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val responseData = response.body?.string()
+                    // Handle the server response and update the TextView with depression level data
+                    // You need to parse the responseData according to the JSON format
+                    // returned by the Flask server.
+                    responseData?.let {
+                        val depressionList = parseDepressionData(it)
+                        updateUI(depressionList)
+                    }
+                }
+            })
+        }
+    }
+
+    private fun parseDepressionData(responseData: String): List<DepressionData> {
+        val depressionList = mutableListOf<DepressionData>()
+        try {
+            val jsonArray = JSONArray(responseData)
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject: JSONObject = jsonArray.getJSONObject(i)
+                val level = jsonObject.getInt("depress")
+                val userId = jsonObject.getInt("user_id")
+                depressionList.add(DepressionData(level, userId))
             }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return depressionList
+    }
+
+    private fun updateUI(depressionList: List<DepressionData>) {
+        // Check if the depressionList is not empty
+        if (depressionList.isNotEmpty()) {
+            // Get the latest data from the depressionList (the latest data is the last one in the list)
+            val latestDepressionData = depressionList.last()
+
+            // Get the depression level from the latest data
+            val depressionLevel = latestDepressionData.level
+
+            // Update the text in depressionLevelTextView with the depression level
+            depressionLevelTextView.text = depressionLevel.toString()
+        } else {
+            // If there is no data, set the text in depressionLevelTextView to an empty message or as needed
+            depressionLevelTextView.text = ""
+        }
     }
 }
--- src/main/java/com/masbin/myhealth/ui/bottom_navigation/home/HomeFragment.kt
+++ src/main/java/com/masbin/myhealth/ui/bottom_navigation/home/HomeFragment.kt
@@ -2,23 +2,32 @@ package com.masbin.myhealth.ui.bottom_navigation.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.masbin.myhealth.databinding.FragmentHomeBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import okhttp3.*
+import okio.IOException
+import org.json.JSONArray
+import org.json.JSONObject
 
 class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    data class DepressionData(val level: Int, val classification:String, val userId: Int)
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
+    ): View? {
         homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
@@ -33,8 +42,89 @@ class HomeFragment : Fragment() {
         return root
     }
 
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Fetch weekly depression history data from the Flask server and display it in the TextView
+        fetchHistoryDataFromServer()
+    }
+
+    private fun fetchHistoryDataFromServer() {
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://beflask.as.r.appspot.com//get/depress/history/week") // Adjust the Flask endpoint for weekly depression history data
+            .build()
+
+        GlobalScope.launch(Dispatchers.IO) {
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    // Handle errors when fetching data from the server
+                    // For example, display an error message or log for debugging
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    val responseData = response.body?.string()
+                    // Handle the server response and update the TextView with depression history data
+                    // You need to parse the responseData according to the JSON format
+                    // returned by the Flask server.
+                    responseData?.let {
+                        val depressionHistoryList = parseDepressionHistoryData(it)
+                        updateUI(depressionHistoryList)
+                    }
+                }
+            })
+        }
+    }
+
+    private fun parseDepressionHistoryData(responseData: String): List<DepressionData> {
+        val depressionHistoryList = mutableListOf<DepressionData>()
+        try {
+            val jsonArray = JSONArray(responseData)
+            for (i in 0 until jsonArray.length()) {
+                val jsonObject: JSONObject = jsonArray.getJSONObject(i)
+                val level = jsonObject.getInt("depress")
+                val classification = jsonObject.getString("classification")
+                val userId = jsonObject.getInt("user_id")
+                depressionHistoryList.add(DepressionData(level,classification, userId))
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return depressionHistoryList
+
+    }
+
+    private fun updateUI(depressionHistoryList: List<DepressionData>) {
+        // Display the weekly history of depression levels in the TextView
+        // Format the data as needed to show the history over the week
+        // For example, you can use a StringBuilder to construct the text to display.
+
+        if (depressionHistoryList.isNotEmpty()) {
+            // Get the latest data from the depressionList (the latest data is the last one in the list)
+            val latestDepressionData = depressionHistoryList.last()
+
+            // Get the depression level from the latest data
+            val depressionLevel = latestDepressionData.level
+            val depressionClassification = latestDepressionData.classification
+
+            activity?.runOnUiThread {
+                // Find the TextView by its ID in the fragment_home.xml and update its text with the depression history data
+                binding.valueDepressReal.text = depressionLevel.toString()
+                binding.tvStatusDepress.text = depressionClassification
+            }
+        } else {
+            // If there is no data, you can handle it here (e.g., show a message)
+            Log.d("HomeFragment", "Depression history list is empty")
+        }
+    }
+
+
+
 }
