491ea655364f301529bdf4a3b665d440395dafc4
muhammadbintangkh <muhammad.bintang.khatulistiwa@hashmicro.com>
2023-08-11 01:45:03 +0700

    password ke hiding change with icon

--- src/main/java/com/masbin/myhealth/ui/bottom_navigation/home/ResetPasswordActivity.kt
+++ src/main/java/com/masbin/myhealth/ui/bottom_navigation/home/ResetPasswordActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.masbin.myhealth.R
 import com.masbin.myhealth.ui.signin.LoginActivity
+import kotlinx.android.synthetic.main.activity_login.togglePassword
+import kotlinx.android.synthetic.main.activity_reset_password.toggleConfirmPassword
+import kotlinx.android.synthetic.main.activity_reset_password.toggleNewPassword
 import org.json.JSONObject
 import java.io.OutputStream
 import java.net.HttpURLConnection
@@ -16,28 +20,52 @@ import java.net.URL
 
 class ResetPasswordActivity : AppCompatActivity() {
     private lateinit var etNewPassword: EditText
+    private lateinit var etConfirmPassword: EditText
     private lateinit var btnUpdatePassword: Button
+    private var isPasswordVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password)
         supportActionBar?.hide()
         etNewPassword = findViewById(R.id.etNewPassword)
+        etConfirmPassword = findViewById(R.id.etConfirmPassword)
         btnUpdatePassword = findViewById(R.id.btnUpdatePassword)
 
         val email = intent.getStringExtra("email")
 
         btnUpdatePassword.setOnClickListener {
             val newPassword = etNewPassword.text.toString().trim()
+            val confirmPassword = etConfirmPassword.text.toString().trim()
 
-            if (newPassword.isEmpty()) {
-                Toast.makeText(this, "Tolong isi password baru kamu", Toast.LENGTH_SHORT).show()
+            if (newPassword.isEmpty() || confirmPassword.isEmpty()) {
+                Toast.makeText(this, "Silakan masukkan kata sandi baru dan konfirmasi kata sandi", Toast.LENGTH_SHORT).show()
+            } else if (newPassword != confirmPassword) {
+                Toast.makeText(this, "Kata sandi baru dan kata sandi konfirmasi tidak cocok", Toast.LENGTH_SHORT).show()
             } else {
                 if (email != null) {
                     updatePasswordOnServer(email, newPassword)
                 }
             }
         }
+
+        // Toggle untuk newPassword
+        toggleNewPassword.setOnCheckedChangeListener { _, isChecked ->
+            isPasswordVisible = isChecked
+            updateNewPasswordVisibility()
+        }
+        // Toggle untuk confirmPassword
+        toggleConfirmPassword.setOnCheckedChangeListener { _, isChecked ->
+            isPasswordVisible = isChecked
+            updateConfirmPasswordVisibility()
+        }
+    }
+
+    private fun updateNewPasswordVisibility() {
+        etNewPassword.transformationMethod = if (isPasswordVisible) null else PasswordTransformationMethod.getInstance()
+    }
+    private fun updateConfirmPasswordVisibility() {
+        etConfirmPassword.transformationMethod = if (isPasswordVisible) null else PasswordTransformationMethod.getInstance()
     }
 
     private fun updatePasswordOnServer(email: String, newPassword: String) {
@@ -105,4 +133,3 @@ class ResetPasswordActivity : AppCompatActivity() {
         }
     }
 }
-
